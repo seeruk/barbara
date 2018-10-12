cd96ce9b0cd569cd8dc6b02d89029f589fd6191e
Elliot Wright <wright.elliot@gmail.com>
2018-10-12 22:57:08 +0100

    Beginning battery module.

---
+++ modules/battery/config.go
@@ -0,0 +1,14 @@
+package battery
+
+// Config holds all battery module configuration.
+type Config struct {
+	// PowerSupply is the name of the power supply found in /sys/class/power_supply, e.g. BAT0. The
+	// power supply must actually be a battery.
+	PowerSupply string `json:"power_supply"`
+}
+
+// Notes:
+// * For me: `cd /sys/class/power_supply/BAT0`
+// * Time remaining to empty: `calc (cat charge_now)" / "(cat current_now)`
+// * Time remaining to full: `calc "("(cat charge_full)" - "(cat charge_now)") / "(cat current_now)`
+// * Current %age: `cat capacity`
