81de4c8a635c96e6d815200772d7db75871fc952
Elliot Wright <wright.elliot@gmail.com>
2018-10-14 15:37:18 +0100

    Battery module not going spectacularly at the moment.

--- modules/battery/config.go
+++ modules/battery/config.go
@@ -5,6 +5,7 @@ type Config struct {
 	// PowerSupply is the name of the power supply found in /sys/class/power_supply, e.g. BAT0. The
 	// power supply must actually be a battery.
 	PowerSupply string `json:"power_supply"`
+	// TODO(elliot): Specifying defaults would be useful for things like refresh interval...
 }
 
 // TODO:
--- modules/battery/module.go
+++ modules/battery/module.go
@@ -25,13 +25,16 @@ type Module struct {
 	ctx context.Context
 	cfn context.CancelFunc
 
-	config Config
-	layout *widgets.QHBoxLayout
-	label  *widgets.QLabel
-	icon   *gui.QIcon
+	config    Config
+	layout    *widgets.QHBoxLayout
+	iconLabel *widgets.QLabel
+	label     *widgets.QLabel
 }
 
 // NewModule returns a new battery Module instance.
+// TODO(elliot): This is a mess. Let's make a service for battery information, then just use this as
+// the UI component for it, moving all of the actual logic out of here...
+// TODO(elliot): If we're at 100%, probably no need to show a time remaining...
 func NewModule(mctx barbara.ModuleContext) (barbara.Module, error) {
 	var config Config
 
@@ -49,10 +52,34 @@ func NewModule(mctx barbara.ModuleContext) (barbara.Module, error) {
 // Render ...
 func (m *Module) Render() (widgets.QLayout_ITF, error) {
 	m.layout = widgets.NewQHBoxLayout()
-	m.label = widgets.NewQLabel(m.layout.Widget(), core.Qt__Widget)
+	m.iconLabel = widgets.NewQLabel(nil, core.Qt__Widget)
+	m.label = widgets.NewQLabel(nil, core.Qt__Widget)
 
 	m.ctx, m.cfn = context.WithCancel(context.Background())
 	m.onTick()
+
+	statusCh := make(chan struct{}, 1)
+
+	go func() {
+		// TODO(elliot): Config.
+		ticker := time.NewTicker(time.Second)
+		oldStatus := m.getBatteryStatus()
+
+		for {
+			select {
+			case <-m.ctx.Done():
+				ticker.Stop()
+				return
+			case <-ticker.C:
+				status := m.getBatteryStatus()
+				if status != oldStatus {
+					statusCh <- struct{}{}
+				}
+
+				oldStatus = status
+			}
+		}
+	}()
 
 	go func() {
 		// TODO(elliot): Config.
@@ -63,12 +90,16 @@ func (m *Module) Render() (widgets.QLayout_ITF, error) {
 			case <-m.ctx.Done():
 				ticker.Stop()
 				return
+			case <-statusCh:
+				fmt.Println("Updating display")
+				m.onTick()
 			case <-ticker.C:
 				m.onTick()
 			}
 		}
 	}()
 
+	m.layout.AddWidget(m.iconLabel, 0, core.Qt__AlignJustify)
 	m.layout.AddWidget(m.label, 0, core.Qt__AlignJustify)
 
 	return m.layout, nil
@@ -88,72 +119,88 @@ func (m *Module) Destroy() error {
 		m.label.Destroy(true, true)
 	}
 
-	if m.icon != nil {
-		m.icon.DestroyQIcon()
+	if m.iconLabel != nil {
+		m.iconLabel.Destroy(true, true)
 	}
 
 	m.ctx = nil
 	m.cfn = nil
 	m.layout = nil
 	m.label = nil
-	m.icon = nil
+	m.iconLabel = nil
 
 	return nil
 }
 
+// onTick ...
 func (m *Module) onTick() {
-	labelText, err := m.getLabelText()
-	if err != nil {
-		return
+	status := m.getBatteryStatus()
+
+	percentage := m.getBatteryPercentage()
+	labelText := m.getLabelText(percentage, status)
+
+	var iconLevel string
+	switch {
+	case percentage == 100:
+		iconLevel = "full"
+	case percentage >= 66:
+		iconLevel = "good"
+	case percentage >= 33:
+		iconLevel = "medium"
+	case percentage > 0:
+		iconLevel = "low"
+	case percentage == 0:
+		iconLevel = "empty"
 	}
 
+	var iconStatus string
+	if status == "charging" {
+		iconStatus = "-charging"
+	}
+
+	icon := gui.NewQIcon5(fmt.Sprintf(
+		"/usr/share/icons/Paper-Mono-Dark/24x24/panel/battery-%s%s.svg",
+		iconLevel,
+		iconStatus,
+	))
+
+	m.iconLabel.SetPixmap(icon.Pixmap2(24, 24, gui.QIcon__Normal, gui.QIcon__On))
 	m.label.SetText(labelText)
 }
 
 // getLabelText ...
-func (m *Module) getLabelText() (string, error) {
-	percentage, err := m.getBatteryPercentage()
-	if err != nil {
-		return "", err
-	}
+func (m *Module) getLabelText(percentage float64, status string) string {
+	timeRemaining := m.getTimeRemaining(status == "charging")
 
-	timeRemaining, err := m.getTimeRemaining(false)
-	if err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf("%s%% (%s)", percentage, timeRemaining), nil
+	return fmt.Sprintf("%.0f%% (%s)", percentage, timeRemaining)
 }
 
 // getBatteryPercentage ...
-func (m *Module) getBatteryPercentage() (string, error) {
-	return fileToStr(fmt.Sprintf("%s/%s/capacity", powerSupplyPath, m.config.PowerSupply))
+func (m *Module) getBatteryPercentage() float64 {
+	return fileToFloat(fmt.Sprintf("%s/%s/capacity", powerSupplyPath, m.config.PowerSupply))
+}
+
+// getBatteryStatus ...
+func (m *Module) getBatteryStatus() string {
+	status, err := fileToStr(fmt.Sprintf("%s/%s/status", powerSupplyPath, m.config.PowerSupply))
+	if err != nil {
+		return "unknown"
+	}
+
+	return strings.ToLower(status)
 }
 
 // getTimeRemaining ...
-func (m *Module) getTimeRemaining(isCharging bool) (string, error) {
+func (m *Module) getTimeRemaining(isCharging bool) string {
 	ps := m.config.PowerSupply
 
-	chargeNow, err := fileToFloat(fmt.Sprintf("%s/%s/charge_now", powerSupplyPath, ps))
-	if err != nil {
-		// TODO(elliot): Context.
-		return "", err
-	}
-
-	currentNow, err := fileToFloat(fmt.Sprintf("%s/%s/current_now", powerSupplyPath, ps))
-	if err != nil {
-		// TODO(elliot): Context.
-		return "", err
-	}
+	chargeNow := fileToFloat(fmt.Sprintf("%s/%s/charge_now", powerSupplyPath, ps))
+	currentNow := fileToFloat(fmt.Sprintf("%s/%s/current_now", powerSupplyPath, ps))
 
 	hours := chargeNow / currentNow
 
 	if isCharging {
-		chargeFull, err := fileToFloat(fmt.Sprintf("%s/%s/charge_full", powerSupplyPath, ps))
-		if err != nil {
-			// TODO(elliot): Context.
-			return "", err
-		}
+		chargeFull := fileToFloat(fmt.Sprintf("%s/%s/charge_full", powerSupplyPath, ps))
 
 		hours = (chargeFull - chargeNow) / currentNow
 	}
@@ -163,7 +210,7 @@ func (m *Module) getTimeRemaining(isCharging bool) (string, error) {
 
 	minutes = duration.Minutes() - (math.Floor(duration.Hours()) * 60)
 
-	return fmt.Sprintf("%02.0f:%02.0f", duration.Hours(), minutes), nil
+	return fmt.Sprintf("%02.0f:%02.0f", duration.Hours(), minutes)
 }
 
 // fileToStr ...
@@ -184,11 +231,18 @@ func fileToStr(fileName string) (string, error) {
 }
 
 // fileToFloat ...
-func fileToFloat(fileName string) (float64, error) {
+func fileToFloat(fileName string) float64 {
+	// TODO(elliot): Log in here probably?
+
 	str, err := fileToStr(fileName)
 	if err != nil {
-		return 0, err
+		return 0
 	}
 
-	return strconv.ParseFloat(str, 64)
+	flt, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return 0
+	}
+
+	return flt
 }
